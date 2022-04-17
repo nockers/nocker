b603667a586e8a01aaeee770c5264eff5431e9da
reiwa <reiwa@outlook.com.vn>
2022-04-18 05:39:35 +0900

    update

--- packages/mui/src/hooks/useResize.ts
+++ packages/mui/src/hooks/useResize.ts
@@ -4,10 +4,13 @@ export const useResize = (eventListener: () => void) => {
   useEffect(() => {
     window.addEventListener("resize", eventListener)
 
-    eventListener()
+    const id = setInterval(() => {
+      eventListener()
+    }, 1000)
 
     return () => {
       window.removeEventListener("resize", eventListener)
+      clearInterval(id)
     }
   }, [])
 }
--- packages/mui/src/utils/initSentry.ts
+++ packages/mui/src/utils/initSentry.ts
@@ -5,7 +5,7 @@ export const initSentry = () => {
     dsn: "https://6e199171fc8e4bc29906ad62cf2178e2@o482319.ingest.sentry.io/6312067",
     tracesSampleRate: 1.0,
     environment: process.env.NODE_ENV ?? "production",
-    release: `knockr-mui@0.3.1`,
+    release: `knockr-mui@0.3.3`,
     beforeSend(event) {
       for (const exception of event.exception?.values ?? []) {
         console.error(exception.value)
