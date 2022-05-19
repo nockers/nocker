3cab2168c9e7a702f05c3b74c065bc30236a906d
reiwa <reiwa@outlook.com.vn>
2022-05-20 02:12:07 +0900

    fix

--- packages/mui/.storybook/main.js
+++ packages/mui/.storybook/main.js
@@ -1,12 +1,18 @@
-module.exports = {
-  stories: ["../src/**/*.stories.tsx"],
-  addons: ["@storybook/addon-essentials"],
-  framework: "@storybook/react",
-  webpackFinal(config) {
-    delete config.resolve.alias["emotion-theming"]
-    delete config.resolve.alias["@emotion/styled"]
-    delete config.resolve.alias["@emotion/core"]
-    return config
-  },
-  staticDirs: ["./public"],
+exports.stories = ["../src/**/*.stories.tsx"]
+
+exports.addons = [
+  "@storybook/addon-links",
+  "@storybook/addon-essentials",
+  "@storybook/addon-interactions",
+]
+
+exports.framework = "@storybook/react"
+
+exports.staticDirs = ["./public"]
+
+exports.webpackFinal = (config) => {
+  delete config.resolve.alias["emotion-theming"]
+  delete config.resolve.alias["@emotion/styled"]
+  delete config.resolve.alias["@emotion/core"]
+  return config
 }
