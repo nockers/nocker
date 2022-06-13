787312af0c72f227f2d0c13a02d033d29ecee152
haruta24 <mptjapw@gmail.com>
2022-06-13 19:08:38 +0900

    update

--- packages/chakra-ui/lib/WidgetTicket.tsx
+++ packages/chakra-ui/lib/WidgetTicket.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@chakra-ui/react"
+import { Button, Stack, Textarea } from "@chakra-ui/react"
 import { WidgetConfig, Ticket } from "@nocker/client"
 import { WidgetTicketSubmit } from "@nocker/react"
 import React, { FC } from "react"
@@ -15,8 +15,13 @@ type Props = {
 
 export const WidgetTicket: FC<Props> = (props) => {
   return (
-    <Box>
-      <Button>{"Hello"}</Button>
-    </Box>
+    <Stack>
+      <Textarea
+        bg={"white"}
+        color={"black"}
+        placeholder="製品の改善についてご意見・ご要望をお聞かせください。"
+      ></Textarea>
+      <Button>{"送信"}</Button>
+    </Stack>
   )
 }
