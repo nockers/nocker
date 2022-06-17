b62c896e9e64dfbe7fa5973ff381f6947e39b8b2
haruta24 <mptjapw@gmail.com>
2022-06-17 18:23:56 +0900

    update

--- packages/browser/lib/utils/initSentry.ts
+++ packages/browser/lib/utils/initSentry.ts
@@ -4,8 +4,8 @@ export const initSentry = () => {
   init({
     dsn: "https://6e199171fc8e4bc29906ad62cf2178e2@o482319.ingest.sentry.io/6312067",
     tracesSampleRate: 1.0,
-    environment: process.env.NODE_ENV ?? "production",
-    release: `nocker-mui@0.4.2`,
+    environment: process?.env?.NODE_ENV ?? "production",
+    release: `browser@0.1.4`,
     beforeSend(event) {
       for (const exception of event.exception?.values ?? []) {
         console.error(exception.value)
