af6728288b58f8e388c2b1484a915d06c1074bbe
reiwa <reiwa@outlook.com.vn>
2022-06-17 20:07:23 +0900

    update

--- packages/browser/lib/utils/initSentry.ts
+++ packages/browser/lib/utils/initSentry.ts
@@ -4,7 +4,8 @@ export const initSentry = () => {
   init({
     dsn: "https://6e199171fc8e4bc29906ad62cf2178e2@o482319.ingest.sentry.io/6312067",
     tracesSampleRate: 1.0,
-    environment: process.env.NODE_ENV ?? "production",
+    // environment: process.env.NODE_ENV ?? "production",
+    environment: "production",
     release: `browser@0.1.4`,
     beforeSend(event) {
       for (const exception of event.exception?.values ?? []) {
